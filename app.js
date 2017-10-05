5b1eb26bd0a53a06195a9b54bb19ceaf67c0b73e
Stephanie Baron <stephaniebaron12@gmail.com>
2017-10-05 13:21:58 -0400

    Add route and params

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res){
   res.send("Happy Thursday!");
 })
 
+app.get('/about/dog', function (req, res) {
+  console.log('This is about/dog');
+})
+
+app.get('/about/:animal', function (req, res) {
+  console.log('This is about/animal');
+})
+
 app.listen(8000, function (){
   console.log("Listening on 8000...");
 })
